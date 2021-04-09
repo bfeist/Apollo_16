466b1b9a38bdce068f2959c9fa65ab70460bc18a
Ben Feist <bf@benfeist.com>
2021-04-09 01:37:24 -0400

    First wack at importing AFJ to csv

--- scripts/scrape_afj_utterances.py
+++ scripts/scrape_afj_utterances.py
@@ -1,26 +1,48 @@
-__author__ = 'Feist'
+__author__ = "Feist"
 import requests
 import re
 
 
 def cleanseString(str):
-    result = re.sub('<a name=".*"></a>', '', str)
-    result = re.sub(' +', ' ', result).strip()
+    result = re.sub('<a name=".*"></a>', "", str)
+    result = re.sub(" +", " ", result).strip()
     return result
 
-urlArray = ["00_crewchange.html", "01launch_ascent.html", "02earth_orbit_tli.html",
-            "03tde.html", "04day1-end.html", "05day2-mcc2-tv.html", "06day2-end.html",
-            "07day3-before-the-storm.html", "08day3-problem.html", "09day3-lifeboat.html",
-            "10day3-free-return.html", "11day3-minimise-power.html", "12day4-approach-moon.html",
-            "13day4-leaving-moon.html", "14day4-homeward.html", "15day4-mailbox.html",
-            "16day4-checkingodyssey.html", "17day5-thumpandsnowflakes.html", "18day5-feelingthecold.html",
-            "19day5-themanualcoursecorrection.html", "20day5-wobblesandbursts.html",
-            "21day5-batterycharge.html", "22day6-packingup.html", "23day6-thereactivationchecklist.html",
-            "24day6-wornoutcrew.html", "25day6-thelastcoursecorrection.html", "26day6-servicemoduleseparation.html",
-            "27day6-odysseyresurrected.html", "28day6-farewellaquarius.html", "29day6-returnhome.html",
-            "30postflight.html"]
 
-# urlArray = ["05day2-mcc2-tv.html"]
+urlArray = [
+    "01_Day1_Pt1.html",
+    "02_Day1_Pt2.html",
+    "03_Day1_Pt3.html",
+    "04_Day1_Pt4.html",
+    "05_Day1_Pt5.html",
+    "06_Day2_Pt1.html",
+    "07_Day2_Pt2.html",
+    "08_Day3_Pt1.html",
+    "09_Day3_Pt2.html",
+    "10_Day4_Pt1.html",
+    "11_Day4_Pt2.html",
+    "12_Day4_Pt3.html",
+    "13_Day5_Pt1.html",
+    "14_Day5_Pt2.html",
+    "15_Day5_pt3.html",
+    "16_Day5_Pt4.html",
+    "17_Day5_Pt5.html",
+    "18_Day5_Pt6.html",
+    "19_Day6_Pt1.html",
+    "20_Day6_Pt2.html",
+    "21_Day7.html",
+    "22_Day8_Pt1.html",
+    "23_Day8_Pt2.html",
+    "24_Day9_Pt1.html",
+    "25_Day9_Pt2.html",
+    "26_Day10_Pt1.html",
+    "27_Day10_Pt2.html",
+    "28_Day11_Pt1.html",
+    "29_Day11_Pt2.html",
+    "30_Day12.html",
+]
+
+# urlArray = ["02_Day1_Pt2.html"]
 
 outputFilePath = "../MISSION_DATA/scraped_data/scraped_utterances_AFJ.csv"
 outputFile = open(outputFilePath, "w")
@@ -29,97 +51,122 @@ outputFile.close()
 outputFile = open(outputFilePath, "a")
 
 for url in urlArray:
-    # request = requests.get('https://history.nasa.gov/afj/ap13fj/' + url)
-    # pageAscii = request.text.encode('ascii', 'ignore').decode('ascii')
-    # lines = pageAscii.split("\r\n")
-    data = open('../ap13fj/' + url, 'r')
+    # request = requests.get("https://history.nasa.gov/afj/ap16fj/" + url)
+    # pageAscii = request.text.encode("ascii", "ignore").decode("ascii")
+    # lines = pageAscii.split("\r")
+    data = open("../ap16fj/" + url, "r")
     pageAscii = data.read()
     lines = pageAscii.split("\n")
 
-    timestamp = ''
-    utterance = ''
-    who = ''
-    utterance_type = ''
+    timestamp = ""
+    utterance = ""
+    who = ""
+    utterance_type = ""
     linecounter = 0
     new_line_to_write = False
+    multiline_start = False
+    startLineNum = 0
 
     for line in lines:
         linecounter += 1
-        if linecounter == 89:
-            print('test area')
-        line_match = re.search(r'(\d{3}:\d{2}:\d{2})', line)
+        if linecounter == 1444:
+            print("test area")
+
+        line_match = re.search(r"<p class=\"?(tech|ob|pao)\"?>.*", line)
         if line_match is not None:
-            timestamp = line_match.group(1)
+            multiline_start = True
 
-        timestamp_match = re.search(r'<b>T-(\d{3}:\d{2}:\d{2})', line)
-        if timestamp_match is not None:
-            timestamp = "-" + timestamp_match.group(1)[1:]
+            line_match = re.search(r"(\d{3}(:| )\d{2}(:| )\d{2})", line)
+            if line_match is not None:
+                timestamp = line_match.group(1)
 
-        line_match = re.search(r'<div.*(<a.*?<\/a>)?<b>(\d{3}:\d{2}:\d{2}) (.*( \(onboard\))?):</b> (.*)<\/div>', line)
-        if line_match is not None:
-            who = line_match.group(3)
-            if "Lovell" in who:
-                who = "CDR"
-            elif "Haise" in who:
-                who = "LMP"
-            elif "Swigert" in who:
-                who = "CMP"
-            utterance = line_match.group(5)
-            utterance_type = "T"
-            new_line_to_write = True
+            timestamp_match = re.search(r"<b>-(\d{3}(:| )\d{2})", line)
+            if timestamp_match is not None:
+                timestamp = "-" + timestamp_match.group(1)[1:] + ":00"
 
-        pao_match = re.search(r'<div class="pao">(.*)</div>', line)
-        if pao_match is not None:
-            who = "PAO"
-            utterance = pao_match.group(1)
-            utterance_type = "P"
-            new_line_to_write = True
+            timestamp_match = re.search(r"<b>-(\d{3}(:| )\d{2}(:| )\d{2})", line)
+            if timestamp_match is not None:
+                timestamp = "-" + timestamp_match.group(1)[1:]
 
-        onboard_match = re.search(r'<div class="onboard">(.*)</div>', line)
-        if onboard_match is not None:
-            utterance_type = "O"
-            new_line_to_write = True
+            timestamp = re.sub(r" ", ":", timestamp)
 
-        cc_match = re.search(r'<div class="cc">(.*)</div>', line)
-        if cc_match is not None:
-            utterance_type = "C"
-            new_line_to_write = True
+            line_match = re.search(
+                r"<p class=\"?(.*?)\"?><b>(<a.*?<\/a>)?-?(\d{3}(:| )\d{2}(:| )?\d{2}?) ?(.*?( \(onboard\))?):? ?<\/b> ?(.*)(<\/p>)",
+                line,
+            )
+            if line_match is not None:
+                who = line_match.group(6)
+                who = re.sub(r" \(onboard\)", "", who)
+                if "Young" in who:
+                    who = "CDR"
+                elif "Duke" in who:
+                    who = "LMP"
+                elif "Mattingly" in who:
+                    who = "CMP"
+                else:
+                    capcoms = [
+                        "Peterson",
+                        "Fullerton",
+                        "Irwin",
+                        "Haise",
+                        "Roosa",
+                        "Mitchell",
+                        "Hartsfield",
+                        "England",
+                        "Overmyer",
+                    ]
+                    if any(x in who for x in capcoms):
+                        utterance_type = "C"
+                        who = who[0:3]
+
+                utterance = line_match.group(8)
+                utterance_type = "T"
+                startLineNum = linecounter
+
+            pao_match = re.search(r"<p class=\"?pao\"?>.*?\"?(.*)\"?", line)
+            if pao_match is not None:
+                who = "PAO"
+                utterance = pao_match.group(1)
+                utterance_type = "P"
+                utterance = re.sub(r"<b>(.*)?Public Affairs Officer:?</b>:?", "", utterance)
+
+            onboard_match = re.search(r"<p class=ob>(.*)</p>", line)
+            if onboard_match is not None:
+                utterance_type = "O"
+
+            cc_match = re.search(r"<p class=cc>(.*)</p>", line)
+            if cc_match is not None:
+                utterance_type = "C"
+
+        if multiline_start:
+            line_match = re.search(r"(.*)<\/p>", line)
+            if line_match is not None:
+                if linecounter != startLineNum:
+                    utterance = utterance + " " + line_match.group(1)
+                new_line_to_write = True
+                multiline_start = False
+            else:
+                if linecounter != startLineNum:
+                    utterance = utterance + " " + line
 
         if new_line_to_write:
-            utterance = re.sub(r'\[((L|l)ong )?(P|p)ause.*?\]', '', utterance)
-            utterance = re.sub(r'\.\.\.', ' - ', utterance)
-            utterance = re.sub(r'\[(G|g)arble.?\]', '...', utterance)
-            utterance = re.sub(r'\.\.\.\.', '...', utterance)
+            utterance = re.sub(r"\[((L|l)ong )?(P|p)ause.*?\]", "", utterance)
+            utterance = re.sub(r"\.\.\.", " - ", utterance)
+            utterance = re.sub(r"\[(G|g)arble.?\]", "...", utterance)
+            utterance = re.sub(r"\[Apollo\]", "", utterance)
+            utterance = re.sub(r"\&nbsp;", " ", utterance)
+            utterance = re.sub(r"\.\.\.\.", "...", utterance)
+            utterance = re.sub(r"  ", " ", utterance)
+            utterance = utterance.strip()
+            utterance = utterance.strip('"')
             utterance = utterance.strip()
             print(str(linecounter) + " " + timestamp + "|" + utterance_type + "|" + who.strip() + "|" + utterance)
             outputFile.write(timestamp + "|" + utterance_type + "|" + who.strip() + "|" + utterance.strip() + "\n")
             new_line_to_write = False
 
-        # if multiline_started:
-        #     closing_match = re.search(r'(.*)</div>', line)
-        #     if closing_match is not None:
-        #         utterance += closing_match.group(1).strip() + " "
-        #         multiline_started = False
-        #         utterance = cleanseString(utterance)
-        #         print(str(linecounter) + " GET: " + timestamp + " Commentary: " + utterance)
-        #         outputFile.write(timestamp + "|" + utterance + "\n")
-        #         utterance = ''
-        #     else:
-        #         utterance += line.strip() + " "
-        #
-        # commentary_match = re.search(r'<div class="comment">(.*)', line)
-        # if commentary_match is not None:
-        #     commentary_single_line_match = re.search(r'<div class="comment">(.*)</div>', line)
-        #     if commentary_single_line_match is not None:
-        #         if 'omm break.' not in commentary_single_line_match.group(1):
-        #             commentary = commentary_single_line_match.group(1).strip()
-        #             commentary = cleanseString(commentary)
-        #             print(str(linecounter) + " GET: " + timestamp + " Commentary: " + commentary)
-        #             outputFile.write(timestamp + "|" + commentary + "\n")
-        #             commentary = ''
-        #     else:
-        #         multiline_started = True
-        #         commentary += commentary_match.group(1).strip() + " "
+            utterance_type = ""
+            who = ""
+            utterance = ""
 
     print("************* DONE page:", url)
 outputFile.close()
