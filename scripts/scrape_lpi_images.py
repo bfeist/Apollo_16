64722e486c78628413aae1a4844525691c139c2d
Ben Feist <bf@benfeist.com>
2021-04-11 17:13:36 -0400

    Added LPI image scraper (unused)

--- scripts/scrape_lpi_images.py
+++ scripts/scrape_lpi_images.py
@@ -1,4 +1,4 @@
-__author__ = 'Feist'
+__author__ = "Feist"
 import csv
 import urllib.request
 import re
@@ -6,70 +6,79 @@ import os
 
 mission_images = []
 
-outputDest = "F:/lpi_mirror"
+outputDest = "E:/lpi_mirror"
 
-inputFilePath = "..\\MISSION_DATA\\A13_photos.csv"
-reader = csv.reader(open(inputFilePath, "rU"), delimiter='|')
-for row in reader:
-    if row[1].startswith('AS13'):
-        mission_images.append([row[0], row[1], row[2], row[3], row[4]])
+inputFilePath = "..\\MISSION_DATA\\scraped_data\\temp\\flickr_listing.txt"
+data = open(inputFilePath, "r", encoding="utf-8")
+pageAscii = data.read()
+lines = pageAscii.split("\n")
 
-for imageitem in mission_images:
-    filename_match = re.search(r'AS13-(\d\d)-(\d\d\d\d.?)', imageitem[1])
+# type = "thumb"
+type = "browse"
+# type = "print"
+
+for line in lines:
+    filename_match = re.search(r"AS16-(\d\d\d)-(\d\d\d\d.?)", line)
     if filename_match is not None:
         rollNum = filename_match.group(1)
         imgNum = filename_match.group(2)
 
-        outputPath = outputDest + '/thumb/AS13/' + rollNum + '/'
-        if not os.path.isdir(outputDest + '/thumb'):
-            os.mkdir(outputDest + '/thumb')
-            if not os.path.isdir(outputDest + '/thumb/AS13'):
-                os.mkdir(outputDest + '/thumb/AS13')
+        outputPath = outputDest + "/" + type + "/AS16/" + rollNum + "/"
+        if not os.path.isdir(outputDest + "/" + type):
+            os.mkdir(outputDest + "/" + type)
+            if not os.path.isdir(outputDest + "/" + type + "/AS16"):
+                os.mkdir(outputDest + "/" + type + "/AS16")
         if not os.path.isdir(outputPath):
             os.mkdir(outputPath)
-        imageURL = 'https://www.lpi.usra.edu/resources/apollo/images/thumb/AS13/' + rollNum + '/' + imgNum + '.jpg';
-        if not os.path.isfile(outputPath + imgNum + '.jpg'):
-            print('downloading: ' + outputPath + imgNum + '.jpg')
-            urllib.request.urlretrieve(imageURL, outputPath + imgNum + '.jpg')
+        imageURL = (
+            "https://www.lpi.usra.edu/resources/apollo/images/" + type + "/AS16/" + rollNum + "/" + imgNum + ".jpg"
+        )
+        if not os.path.isfile(outputPath + imgNum + ".jpg"):
+            print("downloading: " + outputPath + imgNum + ".jpg")
+            try:
+                urllib.request.urlretrieve(imageURL, outputPath + imgNum + ".jpg")
+            except:
+                print("can't download: " + outputPath + imgNum + ".jpg")
+
         else:
-            print('skipping: ' + outputPath + imgNum + '.jpg')
+            print("skipping: " + outputPath + imgNum + ".jpg")
 
-for imageitem in mission_images:
-    filename_match = re.search(r'AS13-(\d\d)-(\d\d\d\d.?)', imageitem[1])
-    if filename_match is not None:
-        rollNum = filename_match.group(1)
-        imgNum = filename_match.group(2)
+# for line in lines:
+#     filename_match = re.search(r"AS16-(\d\d)-(\d\d\d\d.?)", imageitem[1])
+#     if filename_match is not None:
+#         rollNum = filename_match.group(1)
+#         imgNum = filename_match.group(2)
 
-        outputPath = outputDest + '/browse/AS13/' + rollNum + '/'
-        if not os.path.isdir(outputDest + '/browse'):
-            os.mkdir(outputDest + '/browse')
-            if not os.path.isdir(outputDest + '/browse/AS13'):
-                os.mkdir(outputDest + '/browse/AS13')
-        if not os.path.isdir(outputPath):
-            os.mkdir(outputPath)
-        imageURL = 'https://www.lpi.usra.edu/resources/apollo/images/browse/AS13/' + rollNum + '/' + imgNum + '.jpg';
-        if not os.path.isfile(outputPath + imgNum + '.jpg'):
-            print('downloading: ' + outputPath + imgNum + '.jpg')
-            urllib.request.urlretrieve(imageURL, outputPath + imgNum + '.jpg')
-        else:
-            print('skipping: ' + outputPath + imgNum + '.jpg')
+#         outputPath = outputDest + "/browse/AS16/" + rollNum + "/"
+#         if not os.path.isdir(outputDest + "/browse"):
+#             os.mkdir(outputDest + "/browse")
+#             if not os.path.isdir(outputDest + "/browse/AS16"):
+#                 os.mkdir(outputDest + "/browse/AS16")
+#         if not os.path.isdir(outputPath):
+#             os.mkdir(outputPath)
+#         imageURL = "https://www.lpi.usra.edu/resources/apollo/images/browse/AS16/" + rollNum + "/" + imgNum + ".jpg"
+#         if not os.path.isfile(outputPath + imgNum + ".jpg"):
+#             print("downloading: " + outputPath + imgNum + ".jpg")
+#             urllib.request.urlretrieve(imageURL, outputPath + imgNum + ".jpg")
+#         else:
+#             print("skipping: " + outputPath + imgNum + ".jpg")
 
-for imageitem in mission_images:
-    filename_match = re.search(r'AS13-(\d\d)-(\d\d\d\d.?)', imageitem[1])
-    if filename_match is not None:
-        rollNum = filename_match.group(1)
-        imgNum = filename_match.group(2)
+# for line in lines:
+#     filename_match = re.search(r"AS16-(\d\d)-(\d\d\d\d.?)", imageitem[1])
+#     if filename_match is not None:
+#         rollNum = filename_match.group(1)
+#         imgNum = filename_match.group(2)
 
-        outputPath = outputDest + '/print/AS13/' + rollNum + '/'
-        if not os.path.isdir(outputDest + '/print'):
-            os.mkdir(outputDest + '/print')
-            if not os.path.isdir(outputDest + '/print/AS13'):
-                os.mkdir(outputDest + '/print/AS13')
-        if not os.path.isdir(outputPath):
-            os.mkdir(outputPath)
-        imageURL = 'https://www.lpi.usra.edu/resources/apollo/images/print/AS13/' + rollNum + '/' + imgNum + '.jpg';
-        if not os.path.isfile(outputPath + imgNum + '.jpg'):
-            print('downloading: ' + outputPath + imgNum + '.jpg')
-            urllib.request.urlretrieve(imageURL, outputPath + imgNum + '.jpg')
-        else:
-            print('skipping: ' + outputPath + imgNum + '.jpg')
+#         outputPath = outputDest + "/print/AS16/" + rollNum + "/"
+#         if not os.path.isdir(outputDest + "/print"):
+#             os.mkdir(outputDest + "/print")
+#             if not os.path.isdir(outputDest + "/print/AS16"):
+#                 os.mkdir(outputDest + "/print/AS16")
+#         if not os.path.isdir(outputPath):
+#             os.mkdir(outputPath)
+#         imageURL = "https://www.lpi.usra.edu/resources/apollo/images/print/AS16/" + rollNum + "/" + imgNum + ".jpg"
+#         if not os.path.isfile(outputPath + imgNum + ".jpg"):
+#             print("downloading: " + outputPath + imgNum + ".jpg")
+#             urllib.request.urlretrieve(imageURL, outputPath + imgNum + ".jpg")
+#         else:
+#             print("skipping: " + outputPath + imgNum + ".jpg")
